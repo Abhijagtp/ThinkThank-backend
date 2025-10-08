f578da8bd54b9dd330681e1afa30c0ffecb5f262
Abhishek Jagtap <159562929+Abhijagtp@users.noreply.github.com>
2025-10-08 11:33:32 +0530

    updated the requirements.txt and settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -81,7 +81,7 @@ TEMPLATES = [
 ]
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://yourfrontend.com',"http://127.0.0.1:5173","http://localhost:5173",
-    "http://127.0.0.1:5173",]
+    "http://127.0.0.1:5173",'https://thinkthank.onrender.com']
 
 CORS_ALLOW_CREDENTIALS = True
 
