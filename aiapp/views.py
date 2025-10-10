33ca09e8288b725c03d4f74ccd9ce4f1824786b7
Abhishek Jagtap <159562929+Abhijagtp@users.noreply.github.com>
2025-10-10 17:29:50 +0530

    Update views.py

--- aiapp/views.py
+++ aiapp/views.py
@@ -792,8 +792,9 @@ class UserCommentsView(APIView):
 
 # health check -
 from django.http import JsonResponse
+from django.http import HttpResponse
 
 def health_check(request):
-    return JsonResponse({"status": "ok"}, status=200)
+    return HttpResponse("ok", content_type="text/plain")
 
 
